8eeb929866420fbb49fd381e27ab8ebfe7b18103
Amanda Raymond <amandawraymond@gmail.com>
2017-08-26 22:06:16 -0400

    add additem to list service

--- app/assets/javascripts/angular/models/list.js
+++ app/assets/javascripts/angular/models/list.js
@@ -10,12 +10,17 @@
   function ListFunc($resource) {
     var base_url = '/lists/:id'
     var url = base_url + '.json';
+    var add_item_url = base_url + '/add_item.json';
 
     var default_params = {
       id: '@id'
     }
 
     var additional_methods = {
+      addItem: {
+          method: 'PUT',
+          url: add_item_url
+      }
     };
 
     return $resource(url, default_params, additional_methods); 
